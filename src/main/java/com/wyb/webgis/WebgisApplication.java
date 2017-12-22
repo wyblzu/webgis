d337e4a7de9c410f11bf871d8cca0ed2d8e455f1
root <root@localhost.localdomain>
2017-12-22 09:32:36 -0800

    开始创建spring-boot工程

---
+++ src/main/java/com/wyb/webgis/WebgisApplication.java
@@ -0,0 +1,21 @@
+package com.wyb.webgis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class WebgisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebgisApplication.class, args);
+	}
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+		JettyEmbeddedServletContainerFactory factory =
+				new JettyEmbeddedServletContainerFactory();
+		return factory;
+	}
+}
